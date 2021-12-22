9708b5620399923b8eb20d3a2543ce3f95aea060
Milan Zagorac <zagoracm@gmail.com>
2021-12-22 15:47:32 -0500

    added some more components and placeholders

---
+++ src/components/TypesFetch.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TypesFetch = () => {
+    return (
+        <div>
+            <h1>Types Fetch placeholder</h1>
+        </div>
+    );
+};
+
+export default TypesFetch;
---
+++ src/components/TypesSearchForm.js
@@ -0,0 +1,34 @@
+import { ButtonGroup } from "@chakra-ui/react";
+import React from "react";
+
+const TypesSearchForm = () => {
+    return (
+        <div>
+            <h1>Types Search Form Placeholder</h1>
+        </div>
+    );
+};
+
+export default TypesSearchForm;
+
+// Types - use this as the basis of the form options.
+
+// Normal
+// Fire
+// Water
+// Grass
+// Electric
+// Ice
+// Fighting
+// Poison
+// Ground
+// Flying
+// Psychic
+// Bug
+// Rock
+// Ghost
+// Dark
+// Dragon
+// Steel
+// Fairy
+// Shadow
